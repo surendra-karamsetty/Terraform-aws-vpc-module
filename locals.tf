5ce845ef4707dd297374f2e4cd904a843e9d2d24
surendra-karamsetty <surendra3207@gmail.com>
2026-03-07 13:47:07 +0530

    commit changes

--- locals.tf
+++ locals.tf
@@ -22,5 +22,5 @@ locals {
     var.igw_tags
   )
     #by using the slice function we can get the first two avaiable zones. Here we have used 0-> first zone 1->second zone we have given 2 means it is excluse it wont take only first two zones it will take.
-  az_zone = slice(data.aws_availability_zones.available,0,2)
+  az_zone = slice(data.aws_availability_zones.available.names,0,2)
 }
