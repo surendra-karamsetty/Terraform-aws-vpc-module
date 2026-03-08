1b90f68903c4fce31f2826ab71420cbfa6ad3aca
surendra-karamsetty <surendra3207@gmail.com>
2026-03-08 17:12:50 +0530

    commit changes

--- output.tf
+++ output.tf
@@ -5,3 +5,15 @@ output "az-zone" {
 output "vpc_id" {
     value = aws_vpc.main.id
 }
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
